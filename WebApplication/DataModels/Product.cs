db8803bd7a6d55eb6bb1a18dcfac5b968daf0e3a
Foyaz-Ahmed <ahmedfoyaz0000@gmail.com>
2022-05-26 09:42:27 +0600

    Export Excel

---
+++ WebApplication/DataModels/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { set; get; }
+        public double Price { set; get; }
+        public int CategoryId { set; get; }
+        public DateTime PublishedDate { set; get; }
+
+        public string ImageUrl {set; get;}
+
+    }
+}
